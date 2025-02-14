45d5859beae271c48825e5774fa1e4fd7d6a8a9c
PriyankaPatel72 <114109305+PriyankaPatel72@users.noreply.github.com>
2025-02-14 00:24:28 -0500

    Your commit message

--- app/page.tsx
+++ app/page.tsx
@@ -2,15 +2,9 @@ import Envelope from "./Envelope/page";
 import Image from "next/image";
 
 export default function Home() {
-  const heart = Array(10).fill("/images/heart-removebg-preview.png");
 
   return (
-    <div className="valentine-theme" style={{ backgroundColor: 'lightpink' }}>
-      <div className="flex justify-between m4">
-        {heart.map((src, index) => (
-          <Image key={index} src={src} alt="Heart" width={100} height={20} className="heart-image" />
-        ))}
-      </div>
+    <div className="valentine-theme min-h-screen p-6 pb-10 sm:p-8 font-[family-name:var(--font-geist-sans)]" style={{ backgroundColor: 'lightpink' }}>
       <div className="flex flex-col items-center mt-6">
         <h1 className="text-4xl font-bold text-red-500">YOU HAVE A MESSAGE</h1>
         <Envelope />
