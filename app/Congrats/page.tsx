a93e9ce02dd393b1ee058690113f88f2e9d0cc24
PriyankaPatel72 <114109305+PriyankaPatel72@users.noreply.github.com>
2025-02-13 23:45:03 -0500

    Your commit message

--- app/Congrats/page.tsx
+++ app/Congrats/page.tsx
@@ -1,16 +1,9 @@
 'use client'
 import { useEffect } from 'react';
 import Image from 'next/image';
-import confetti from 'canvas-confetti';
 
 export default function CongratsPage() {
   useEffect(() => {
-    confetti({
-      particleCount: 200,
-      spread: 70,
-      origin: { y: 0.6 },
-    });
-
     const audio = new Audio('/sounds/happy-music.mp3');
     audio.play();
   }, []);
